cf56e7a63ded43961e24b2dbf5e1a32dd9bb7f05
Subhajit Sahu <wolfram77@gmail.com>
2019-04-13 03:13:20 +0530

    save

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const E = process.env;
 const PORT = parseInt(E['PORT']||'8000', 10);
 const ASSETS = path.join(__dirname, 'assets');
 const DEVICE = E['DEVICE']||'http://127.0.0.1:8000';
+const DATARATE = parseInt(E['DATARATE']||'10000', 10);
 const IMAGES = [
   'ias-distancesensor', 'ias-sonarsensor', 'ias-floweranalysissensor',
   'ias-navalminemodel', 'ias-irismodel', 'ias-distancealarmservice',
@@ -16,7 +17,11 @@ const IMAGES = [
 ];
 const app = express();
 const server = http.createServer(app);
-
+var distancesensor, sonarsensor, floweranalysissensor;
+var navalminemodel, irismodel;
+var distancealarmservice, emergencynotificationservice;
+var counterservice, irishelperservice;
+var distancealarm, navalmine, counter, dtime = new Date();
 
 
 async function imageContainer(img) {
@@ -41,27 +46,43 @@ async function appReady() {
 }
 
 async function appMaintain() {
-  var distancesensor = await containerMaintain('ias-distancesensor');
-  var sonarsensor = await containerMaintain('ias-sonarsensor');
-  var floweranalysissensor = await containerMaintain('ias-floweranalysissensor');
-  var navalminemodel = await containerMaintain('ias-navalminemodel');
-  var irismodel = await containerMaintain('ias-irismodel');
-  var distancealarmservice = await containerMaintain('ias-distancealarmservice', {env: {
+  distancesensor = await containerMaintain('ias-distancesensor');
+  sonarsensor = await containerMaintain('ias-sonarsensor');
+  floweranalysissensor = await containerMaintain('ias-floweranalysissensor');
+  navalminemodel = await containerMaintain('ias-navalminemodel');
+  irismodel = await containerMaintain('ias-irismodel');
+  distancealarmservice = await containerMaintain('ias-distancealarmservice', {env: {
     SOURCE: `http://${distancesensor.env.ADDRESS}/status`,
     TARGET: `http://${ADDRESS}/distancealarm`,
   }});
-  var emergencynotificationservice = await containerMaintain('ias-emergencynotificationservice', {env: {
+  emergencynotificationservice = await containerMaintain('ias-emergencynotificationservice', {env: {
     SOURCE: `http://${distancesensor.env.ADDRESS}/status`,
     // MAIL
   }});
-  var counterservice = await containerMaintain('ias-counterservice', {env: {
+  counterservice = await containerMaintain('ias-counterservice', {env: {
     TARGET: `http://${ADDRESS}/counter`,
   }});
-  var irishelperservice = await containerMaintain('ias-irishelperservice', {env: {
+  irishelperservice = await containerMaintain('ias-irishelperservice', {env: {
     SOURCE: `http://${floweranalysissensor.env.ADDRESS}/status`,
-    MODEL: `http://${irismodel.env.ADDRESS}/v1/models/model`,
+    MODEL: `http://${irismodel.env.ADDRESS.split()[1]}/v1/models/model`,
   }});
 }
+
+function onInterval() {
+  if(!sonarsensor) return;
+  var res = await needle('get', `http://${sonarsensor.env.ADDRESS}/status`);
+  console.log('sonarsensor', sonarsensor, res.body);
+  var {time, inputs} = res.body;
+  if(!navalminemodel) return;
+  var data = {examples: [{inputs: [inputs]}]};
+  res = await needle('post', `http://${navalminemodel.env.ADDRESS.split()[1]}/v1/models/model:classify`, data, {json: true});
+  console.log('navalminemodel', navalminemodel.env.ADDRESS, res.body);
+  var {results} = res.body;
+  results[0].sort((a, b) => b[1]-a[1]);
+  navalmine = results[0][0][0];
+  console.log('navalminestatus', navalmine);
+}
+setInterval(onInterval, DATARATE);
 
 
 app.use(express.urlencoded({extended: true}));
@@ -71,6 +92,20 @@ app.use((req, res, next) => {
   var {ip, method, url, body} = req;
   if(method!=='GET') console.log(ip, method, url, body);
   next();
+});
+
+app.get('/status', (req, res) => {
+  res.json({time: dtime, distancealarm, navalmine, counter});
+});
+app.post('/distancealarm', (req, res) => {
+  var {time, status} = req.body;
+  dtime = time||new Date();
+  distancealarm = status;
+});
+app.post('/counter', (req, res) => {
+  var {time, count} = req.body;
+  dtime = time||new Date();
+  counter = count;
 });
 
 app.use(express.static(ASSETS, {extensions: ['html']}));
